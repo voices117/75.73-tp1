d387bba3cb55d835421b12df109f2a579d123ff8
axl <axel.lijdens@gmail.com>
2019-09-17 23:49:53 -0300

    agrego endpoints 'intensive' para simular request de carga (y newline al final de las respuestas)

--- js/app.js
+++ js/app.js
@@ -4,13 +4,19 @@ const PORT = 3000;
 const app = express();
 
 app.get('/', (req, res) => {
-  res.status(200).send("Ping");
+  res.status(200).send("node - ping\n");
 });
 
 app.get('/timeout', (req, res) => {
   setTimeout(() => {
-      res.status(200).send("Ping");
+      res.status(200).send("node - timeout\n");
   }, 5000);
+});
+
+app.get('/intensive', (req, res) => {
+  var timeout = new Date(new Date().getTime() + 5000);
+  while( timeout > new Date() ){}
+  res.status(200).send("node - intensive\n");
 });
 
 
--- py/app.py
+++ py/app.py
@@ -5,12 +5,19 @@ app = Flask(__name__)
 
 @app.route("/")
 def ping():
-    return "python - ping"
+    return "python - ping\n"
 
 @app.route("/timeout")
 def timeout():
     time.sleep(5)
-    return "python - timeout"
+    return "python - timeout\n"
+
+@app.route("/intensive")
+def intensive():
+    timeout = time.time() + 5
+    while timeout > time.time():
+        pass
+    return "python - intensive\n"
 
 
 if __name__ == '__main__':
