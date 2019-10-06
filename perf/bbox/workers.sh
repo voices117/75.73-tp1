e83455b881b63b07fea6902c2d3f539fb084f57f
Luis Tejerina <lteje11@gmail.com>
2019-10-06 15:09:24 -0300

    Bbox final

--- perf/bbox/workers.sh
+++ perf/bbox/workers.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 COUNTER=0
-while [ "$COUNTER" -ne "15" ]
+while [ "$COUNTER" -ne "$2" ]
 do
-    curl -w "time_total:  %{time_total}\n" -o /dev/null -s "http://localhost:5555/bbox-2/" &
+    curl -w "time_total:  %{time_total}\n" -o /dev/null -s "http://localhost:5555/$1/" &
     COUNTER=$[$COUNTER +1]
 done
